50763368e140a9de86470aca8df3cdb17a2e0c87
Everaldo Junior <everaldojunior408@gmail.com>
2026-05-20 21:24:38 -0300

    first commit

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,47 @@
+/* console.log('Boas vindas!');
+let nome = 'Junior';
+alert(`Olá, ${nome}`);
+let linguagem = prompt('Qual a linguagem de programação que você mais gosta?');
+console.log(linguagem);
+ */
+
+/* let valor1 = 2;
+let valor2 = 5;
+let resultado = valor1 - valor2;
+console.log(`A soma de ${valor1} e ${valor2} é igual a ${resultado}`); */
+
+/* let idade = prompt('Qual a sua idade?');
+
+if(idade >= 18){
+    console.log('Você é maior de idade!')
+} else{
+    console.log('Você é menor de idade!')
+} */
+
+/* let numero = prompt('Escreva um valor');
+if(numero == 0){
+    alert('Numero zero')
+}else{
+    if(numero > 0){
+        alert('Numero positivo')
+    }else{
+        alert('Numero negativo')
+    }
+} */
+
+/* let numeroInicial = 0;
+while(numeroInicial < 10){
+    numeroInicial++;
+    console.log(numeroInicial);
+} */
+
+/* let nota = 5;
+if(nota > 7){
+    console.log('Aprovado');
+}else{
+    console.log('Repovrado');
+}
+ */
+
+/* console.log(parseInt(Math.random() * 1000 + 1)); */
+
